fe6e71c448b46e61c51cc6a8a767db7aae3d03fa
Andrei Palangean <palangeanad@yahoo.com>
2016-06-14 09:02:35 +0300

    interfata

--- Proiect SEM2/Source.cpp
+++ Proiect SEM2/Source.cpp
@@ -1,1 +1,76 @@
-#include<Windows.h>
+
+
+#include <Windows.h>
+#include <CommCtrl.h>
+#include <tchar.h>
+#include "resource.h"
+
+#pragma comment(linker, \
+  "\"/manifestdependency:type='Win32' "\
+  "name='Microsoft.Windows.Common-Controls' "\
+  "version='6.0.0.0' "\
+  "processorArchitecture='*' "\
+  "publicKeyToken='6595b64144ccf1df' "\
+  "language='*'\"")
+
+#pragma comment(lib, "ComCtl32.lib")
+
+TCHAR text1[255],text2[255];
+
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDCANCEL:
+			SendMessage(hDlg, WM_CLOSE, 0, 0);
+			return TRUE;
+		}
+	case IDC_BUTTON1:
+	{
+		GetDlgItemText(hDlg, IDC_EDIT1, text1, 255);
+		GetDlgItemText(hDlg, IDC_EDIT5, text2, 255);
+
+	}
+		break;
+
+	case WM_CLOSE:
+		if (MessageBox(hDlg, TEXT("Close the program?"), TEXT("Close"),
+			MB_ICONQUESTION | MB_YESNO) == IDYES)
+		{
+			DestroyWindow(hDlg);
+		}
+		return TRUE;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE h0, LPTSTR lpCmdLine, int nCmdShow)
+{
+	HWND hDlg;
+	MSG msg;
+	BOOL ret;
+
+	InitCommonControls();
+	hDlg = CreateDialogParam(hInst, MAKEINTRESOURCE(IDD_DIALOG1), 0, DialogProc, 0);
+	ShowWindow(hDlg, nCmdShow);
+
+	while ((ret = GetMessage(&msg, 0, 0, 0)) != 0) {
+		if (ret == -1)
+			return -1;
+
+		if (!IsDialogMessage(hDlg, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return 0;
+};
